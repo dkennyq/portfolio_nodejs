6a1a92a15745b5a23cb7999819bba52f13d1d7d2
Kenny Q <dev.kennyq@gmail.com>
2024-04-10 21:48:46 -0500

    remove folders

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    aws_table_name: 'zem_user_portfolio',
-    aws_remote_config: {
-         accessKeyId: "AKIA5FTY7C2T5SGVMJ76",
-         secretAccessKey: "63eMhUglcJfo9Xc14HWzELRBeudZp6JK1Aj/G/t",
-         region: 'us-east-2',
-    }
-};
